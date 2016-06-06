690adb21aa496038d567730de71506a766f31901
RonenZubkov <ronenownz@gmail.com>
2016-06-06 10:09:10 +0300

    changed the design abit, still cant ceneter that H1.
    added counter.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
 // visual
 // keep max score in localStorage
 // levels
+
  
 
 'use strict';
@@ -18,121 +19,106 @@ var note2 = new Audio('sound/note2.wav');
 var note3 = new Audio('sound/note3.wav');
 var note4 = new Audio('sound/note4.wav');
 
-
 var NOTES = [
     {sound: note1},
     {sound: note2},
     {sound: note3},
     {sound: note4}
-]
-
+            ];
 
 // This is my State:
 var gState = {
     isUserTurn : false,
-    seqNoteIndexes: [],
-    currNoteIndexToClick: 0
+    playedNotes: [],
+    currNoteToClick: 0
 }
-
 
 function init() {
-  // NOTES = createNotesModel(4);
-    //renderPiano(NOTES);
-    computerTurn();
+
+    computerTurn();     // 
 }
-
-// function createNotesModel(size){
-//     var notes = [];
-//
-//     for (var i = 0; i < size; i++) {
-//        //var note = {sound : 'Note' + (i+1)};
-//         //A test to see if Note is a string.
-//         testString(note);
-//        notes.push(note);
-//     }
-//     return notes;
-// }
-
 
 function addRandomNote() {
-    gState.seqNoteIndexes.push(getRandomIntInclusive(0,NOTES.length-1));
+    gState.playedNotes.push(getRandomIntInclusive(0,NOTES.length-1));
 }
 
-
-function playSeq() {
+function playNotes() {
     
-    var elNotes = document.querySelectorAll('.note');
-    //console.log(elNotes);
-    gState.seqNoteIndexes.forEach(function (seqNoteIndex, i) {
-        
-        setTimeout(function playNote() {
-            elNotes[seqNoteIndex].classList.add('playing');
-            var noteToPlay = NOTES[i].sound;
-            noteToPlay.play();
-
-            setTimeout(function donePlayingNote() {
-                elNotes[seqNoteIndex].classList.remove('playing');
-
-                // A test to check, if class remains!
-                testCondition((elNotes[seqNoteIndex].classList.contains('playing')),'class .playing wasnt dropped')
-            }, 500);
-
-          //  console.log('Playing: ', NOTES[seqNoteIndex].sound);
+    gState.playedNotes.forEach(function (playedNote, i) {
+        var elNote = document.querySelector('[data-note="'+ playedNote+'"]');;
+        setTimeout(function () {
+            playNote(playedNote, elNote);
         }, 1000 * i);
-
 
     });
     
     setTimeout(function () {
         console.log('Done Playing Sequence!!');
         gState.isUserTurn = true;
-    }, 1000 * gState.seqNoteIndexes.length);
+    }, 1000 * gState.playedNotes.length);
    
 }
 
+function playNote(playedNote, elNote) {
+    NOTES[playedNote].sound.play();
+    elNote.classList.add('playing');
+    
+    setTimeout(function donePlayingNote() {
+        elNote.classList.remove('playing');
+    }, 500);
+     // A test to check, if class remains!
+    testCondition((elNote.classList.contains('.playing')),'class .playing wasnt dropped');
+}
+
 function noteClicked(elNote) {
-    
+    var noteCounter = document.querySelector('.count');
+
+
     if (!gState.isUserTurn) return;
-    var noteIndex = +elNote.getAttribute('data-note');
-    console.log('noteIndex is: ', noteIndex);
-    
+    var playedNote = +elNote.getAttribute('data-note');
+    console.log('playedNote is: ', playedNote);
     
     // User clicked the right note
-    if (noteIndex === gState.seqNoteIndexes[gState.currNoteIndexToClick]) {
+    if (playedNote === gState.playedNotes[gState.currNoteToClick]) {
+        playNote(playedNote, elNote);
         console.log('User OK!');
-        gState.currNoteIndexToClick++;
+        gState.currNoteToClick++;
+        console.log(gState.currNoteToClick);
+        noteCounter.innerHTML = gState.currNoteToClick;
         
-        if (gState.currNoteIndexToClick === gState.seqNoteIndexes.length) {
-            computerTurn();
-        }
-        
+        if (gState.currNoteToClick === gState.playedNotes.length) {
+            console.log('well played!');
+            setTimeout(computerTurn, 2000);
+            
+        }  
         
     } else {
         console.log('User Wrong!');
-        var elPiano = document.querySelector('.piano');
+        var elPiano = document.querySelector('.simon');
         elPiano.style.display = 'none';
         //Is elPiano a string.
         testString(elPiano);
-        testCondition((elPiano.style.display === 'none'),'elPiano is is hidden, user lost');
+        testCondition((elPiano.style.display === 'none'),'elPiano is hidden, user lost');
         
     }
     
     // console.log('elNote', elNote);
-    console.log('Note', NOTES[noteIndex]);
+    // console.log('Note', NOTES[noteIndex]);
 
 }
 
 function computerTurn() {
 
-     gState.isUserTurn = false;
-     gState.currNoteIndexToClick  = 0;
+    gState.isUserTurn = false;
+    gState.currNoteToClick  = 0;
+    //alert('User Turn is Over');
+    
+    addRandomNote();
+    playNotes();
 
     //Checking end of users turn.
     testCondition((gState.isUserTurn === true), 'Users turn isnt off!');
-
-     addRandomNote();
-     playSeq();
-    console.log(playSeq());
+    
 }
 
 
